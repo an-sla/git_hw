a5fc68c4de0cec45b46bcc4b085f48162fb84a61
Anastasia Slabucho <anastasia.slabucho@gmail.com>
2022-05-26 18:22:59 +0200

    task24

--- task24/setup.sh
+++ task24/setup.sh
@@ -1,51 +1,26 @@
 #!/bin/bash
-#Include utils
+
+# Include utils
 source ../utils/utils.sh
 
-make-bare-remote-repo 
-clone-remote-to-exercise
-
-git commit --allow-empty -m "Initial commit"
-
-git tag v0.0
-
-git push origin main
-git push origin v0.0
-
-echo "Hello" > hello.code
-git add hello.code
-git commit -m "Helo Volrd feature"
+kata="kata3-detached-head"
+make-exercise-repo
 
 
-echo "HelloWrld?" > hello.code
-echo "Unrelated stuff!" > other.code
-git add -A
-git commit -am "Finished HW feature"
+touch file1
+git add file1
+git commit -am "A"
 
-echo "Hello World!" > hello.code
-git commit -am "Really made the thingy done"
+touch file2
+git add file2
+git commit -am "B"
 
-echo "println DEBUG" >> hello.code
-git commit -am "debugging"
+touch file3
+git add file3
+git commit -am "C"
 
-echo "4321pass" > private.secret
-git add private.secret
-git commit -m "important secret"
+touch file4
+git add file4
+git commit -am "D"
 
-echo "# THE Hello World program" > README.md
-git add README.md
-git commit -m "Add doc - step 1"
-
-echo "# THE Ultimate Hello World program" > README.md
-git commit -am "Add doc - step 2"
-
-echo "" >> README.md
-echo "This program does exactly what it says" >> README.md
-git commit -am "Add doc - step 3"
-
-echo "does_it_work(hello.code)" > hello.test
-git add hello.test
-git commit -m "Test for feature hello world"
-
-echo "does_it_work(hello.code);" > hello.test
-git commit -am "I forgot a semicolon"
+git checkout HEAD~3
