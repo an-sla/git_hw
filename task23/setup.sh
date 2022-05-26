cf2420d5d5c46eaf71751f1fcf7adc699ddac458
Anastasia Slabucho <anastasia.slabucho@gmail.com>
2022-05-26 17:37:23 +0200

    task23

--- task23/setup.sh
+++ task23/setup.sh
@@ -1,51 +1,31 @@
-#!/bin/bash
-#Include utils
+kata="kata6-save-my-commit"
+
+# Include utils
 source ../utils/utils.sh
 
-make-bare-remote-repo 
-clone-remote-to-exercise
+make-exercise-repo
 
-git commit --allow-empty -m "Initial commit"
+echo "initial" > thing.txt
+git add thing.txt
+git commit -m "Initial commit"
+git tag initial-commit
 
-git tag v0.0
+echo "This is a relevant fact" > file.txt
 
-git push origin main
-git push origin v0.0
+git add file.txt
 
-echo "Hello" > hello.code
-git add hello.code
-git commit -m "Helo Volrd feature"
+git commit -m "add relevant fact"
 
+echo "This is an indispensable truth!" > file.txt
 
-echo "HelloWrld?" > hello.code
-echo "Unrelated stuff!" > other.code
-git add -A
-git commit -am "Finished HW feature"
+git add file.txt
 
-echo "Hello World!" > hello.code
-git commit -am "Really made the thingy done"
+git commit -m "add indispensable truth!"
 
-echo "println DEBUG" >> hello.code
-git commit -am "debugging"
+echo "42" > holygrail.txt
 
-echo "4321pass" > private.secret
-git add private.secret
-git commit -m "important secret"
+git add holygrail.txt
 
-echo "# THE Hello World program" > README.md
-git add README.md
-git commit -m "Add doc - step 1"
+git commit -m "found the holy grail"
 
-echo "# THE Ultimate Hello World program" > README.md
-git commit -am "Add doc - step 2"
-
-echo "" >> README.md
-echo "This program does exactly what it says" >> README.md
-git commit -am "Add doc - step 3"
-
-echo "does_it_work(hello.code)" > hello.test
-git add hello.test
-git commit -m "Test for feature hello world"
-
-echo "does_it_work(hello.code);" > hello.test
-git commit -am "I forgot a semicolon"
+git reset --hard HEAD~3
